62cbec3143d6665c473a428fe40e48975c18ee41
Nayden Gochev <gochev@gmail.com>
2015-03-13 21:38:02 +0200

    Adding IN MEMORY authentication for admin pages
    everything on /admin is now password protected.
    
    Use username "admin" and [assword "password" for now to login

--- src/main/java/site/app/SecurityConfig.java
+++ src/main/java/site/app/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -41,9 +42,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      protected void configure(final HttpSecurity http) throws Exception {
 		 http
          .authorizeRequests()
-           .antMatchers("/","/login","/about").permitAll() // #4
-           //.antMatchers("/admin/**").hasRole("ADMIN") // #6
-           //.anyRequest().authenticated() // 7
+           .antMatchers("/","/login","/about", "/nav/**").permitAll() // #4
+           .antMatchers("/admin/**").hasRole("ADMIN") // #6
+           .anyRequest().authenticated() // 7
            .and()
        .formLogin()  // #8
            .loginPage("/login") // #9
