191243ac6ff9340c6380022349b0fc9bc11df0ae
mist <mihail@sty-consulting.com>
2015-04-15 20:12:49 +0300

    more comments

--- src/main/java/site/app/Application.java
+++ src/main/java/site/app/Application.java
@@ -1,6 +1,9 @@
 package site.app;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +11,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -16,6 +20,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * to make this deployable as war, this is necessary:
@@ -95,6 +100,40 @@ public class Application  extends SpringBootServletInitializer {
 //        ds.addDataSourceProperty("prepStmtCacheSqlLimit", 2048);
 //        ds.addDataSourceProperty("useServerPrepStmts", true);
 //        return ds;
+//    }
+
+
+//    @Value("${spring.datasource.username}")
+//    private String user;
+//
+//    @Value("${spring.datasource.password}")
+//    private String password;
+//
+//    @Value("${spring.datasource.url}")
+//    private String dataSourceUrl;
+//
+//    @Value("${spring.datasource.driverClassName}")
+//    private String driverClassName;
+//
+////    @Value("${spring.datasource.connectionTestQuery}")
+////    private String connectionTestQuery;
+//
+//    @Bean
+//    public DataSource primaryDataSource() {
+//        Properties dsProps = new Properties();
+//        dsProps.setProperty("url", dataSourceUrl);
+//        dsProps.setProperty("user", user);
+//        dsProps.setProperty("password", password);
+//
+//        Properties configProps = new Properties();
+////        configProps.setProperty("connectionTestQuery", connectionTestQuery);
+//        configProps.setProperty("driverClassName", driverClassName);
+//        configProps.setProperty("jdbcUrl", dataSourceUrl);
+//
+//        HikariConfig hc = new HikariConfig(configProps);
+//        hc.setDataSourceProperties(dsProps);
+////        hc.setMetricRegistry(metricRegistry);
+//        return new HikariDataSource(hc);
 //    }
 
 }
