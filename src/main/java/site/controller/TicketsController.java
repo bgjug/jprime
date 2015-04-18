7b463a382c5e0e95f200c3c1ccad02fe774c0928
mist <mihail@sty-consulting.com>
2015-04-19 02:04:05 +0300

    the epay receiver

--- src/main/java/site/controller/TicketsController.java
+++ src/main/java/site/controller/TicketsController.java
@@ -7,10 +7,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import site.facade.UserFacade;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 /**
- * @author Ivan St. Ivanov
+ * @author Mihail
  */
 @Controller
 public class TicketsController {
@@ -27,6 +31,26 @@ public class TicketsController {
     public String submissionForm(Model model) {
     	model.addAttribute("tags", userFacade.findAllTags());
         return TICKETS_JSP;
+    }
+
+    /**
+     * Receiving data from epay.bg
+     */
+    @RequestMapping(value = "/tickets/from.epay", method = RequestMethod.POST)
+    @ResponseBody//we return the string literal
+    public String receiveFromEpay(HttpServletRequest request) {
+        Map<String, String[]> parameters = request.getParameterMap();
+
+        for (String key : parameters.keySet()) {
+            System.out.print(key);
+            String[] vals = parameters.get(key);
+            for (String val : vals)
+                System.out.println(" -> " + val);
+        }
+//        for(Map.Entry entry: model.asMap().entrySet()) {
+//            System.out.println("FROM EPAY:"+entry.getKey()+ " " + entry.getValue() );
+//        }
+        return "OK";
     }
 
 }
