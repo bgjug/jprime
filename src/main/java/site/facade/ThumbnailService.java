4014f03919e87017d3e70453857f33e4372ce9c5
Ivan St. Ivanov <ivan_st_ivanov@yahoo.com>
2015-05-11 15:44:47 +0300

    Disable temporarily automatic making of thumbnails

--- src/main/java/site/facade/ThumbnailService.java
+++ src/main/java/site/facade/ThumbnailService.java
@@ -20,22 +20,22 @@ public class ThumbnailService {
 	}
 
 	public byte[] thumbImage(byte[] initialImageInBytes, int widthParam, int heigthParam) {
-		InputStream in = new ByteArrayInputStream(initialImageInBytes);
-		byte [] readyImageInBytes = null;
-		try {
-			BufferedImage tempImage = ImageIO.read(in);
-			BufferedImage readyImage = null;
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			readyImage = Thumbnails.of(tempImage).size(widthParam, heigthParam)
-					.outputFormat("jpg").outputQuality(0.8).asBufferedImage();
-			ImageIO.write(readyImage, "jpg", baos);
-			baos.flush();
-			readyImageInBytes = baos.toByteArray();
-			baos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return readyImageInBytes;
+//		InputStream in = new ByteArrayInputStream(initialImageInBytes);
+//		byte [] readyImageInBytes = null;
+//		try {
+//			BufferedImage tempImage = ImageIO.read(in);
+//			BufferedImage readyImage = null;
+//			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//			readyImage = Thumbnails.of(tempImage).size(widthParam, heigthParam)
+//					.outputFormat("jpg").outputQuality(0.8).asBufferedImage();
+//			ImageIO.write(readyImage, "jpg", baos);
+//			baos.flush();
+//			readyImageInBytes = baos.toByteArray();
+//			baos.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+		return initialImageInBytes;
 	}
 }
 
