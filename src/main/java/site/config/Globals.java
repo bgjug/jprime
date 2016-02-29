51548ef25af215867e126087079d1b796e2a5452
Nayden Gochev <gochev@gmail.com>
2016-02-29 17:49:57 +0200

    git reset of 659c866899068f75b9d60e6821f7e787e42c7567

--- src/main/java/site/config/Globals.java
+++ src/main/java/site/config/Globals.java
@@ -5,5 +5,5 @@ import site.model.Branch;
 
 public class Globals {
 	public static final Branch CURRENT_BRANCH = Branch.YEAR_2016;
-	public static final String CFP = CfpController.CFP_CLOSED_JSP;
+	public static final String CFP = CfpController.CFP_OPEN_JSP;
 }
