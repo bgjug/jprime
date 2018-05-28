1d61711493ae0cdb06137d6d4b65d8d76dfbf071
Nayden Gochev <nayden.gochev@novarto.com>
2018-05-28 16:48:27 +0300

    Closing Registration 2018

--- src/main/java/site/config/Globals.java
+++ src/main/java/site/config/Globals.java
@@ -7,5 +7,5 @@ import site.model.Branch;
 public class Globals {
 	public static final Branch CURRENT_BRANCH = Branch.YEAR_2018;
 	public static final String PAGE_CFP = CfpController.CFP_CLOSED_JSP;
-	public static final String PAGE_TICKETS = TicketsController.TICKETS_REGISTER_JSP;
+	public static final String PAGE_TICKETS = TicketsController.TICKETS_END_JSP;
 }
