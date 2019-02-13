9a4c8500d0c70e2a0b9d7b9f54c762e578efdc32
Doychin Bondzhev <doychin@dsoft-bg.com>
2019-02-13 17:46:05 +0200

    Cleanup unused imports
    Signed-off-by: Doychin Bondzhev <doychin@dsoft-bg.com>

--- src/main/java/site/controller/AdminSponsorController.java
+++ src/main/java/site/controller/AdminSponsorController.java
@@ -14,12 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import site.facade.AdminService;
 import site.facade.ThumbnailService;
-import site.model.Speaker;
 import site.model.Sponsor;
 
 @Controller()
