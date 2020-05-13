d2a61b442a7311cb5112c962b72180a26b8293f8
dalexandrov <mitiaalexandrov@gmail.com>
2020-05-13 13:42:53 +0300

    fixed counter

--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -19,7 +19,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2020/05/27 08:00:00',function(event){
+     jQuery('#clock').countdown('2020/09/09 08:00:00',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
