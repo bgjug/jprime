d0ed360cf6fc9302e9f71762af7dbd7e71995f2f
gochev <gochev@gmail.com>
2021-09-10 10:52:59 +0300

    Adding branch for 2022 instead of using the 2020 one.

--- src/main/java/site/model/Branch.java
+++ src/main/java/site/model/Branch.java
@@ -3,9 +3,13 @@ package site.model;
 import org.joda.time.DateTime;
 
 public enum Branch {
-    YEAR_2015("2015", DateTime.parse("2015-05-27")), YEAR_2016("2016", DateTime.parse("2016-05-28")),
-    YEAR_2017("2017", DateTime.parse("2017-05-30")), YEAR_2018("2018", DateTime.parse("2018-05-29")),
-    YEAR_2019("2019", DateTime.parse("2019-05-28")), YEAR_2020("2020", DateTime.parse("2020-05-27"));
+    YEAR_2015("2015", DateTime.parse("2015-05-27")),
+    YEAR_2016("2016", DateTime.parse("2016-05-28")),
+    YEAR_2017("2017", DateTime.parse("2017-05-30")),
+    YEAR_2018("2018", DateTime.parse("2018-05-29")),
+    YEAR_2019("2019", DateTime.parse("2019-05-28")),
+    YEAR_2020("2020", DateTime.parse("2020-05-27")),
+    YEAR_2022("2022", DateTime.parse("2022-05-25"));
 
     private static final String BRANCH_PREFIX = "YEAR_";
 
