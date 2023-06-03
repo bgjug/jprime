e258db7ec7c4c6047aa316be7a1c3d8d358b430b
doychin <doychin@dsoft-bg.com>
2023-06-03 19:11:57 +0300

    Upgrade to Java 17
    Signed-off-by: doychin <doychin@dsoft-bg.com>

--- src/main/java/site/controller/ticket/TicketExporter.java
+++ src/main/java/site/controller/ticket/TicketExporter.java
@@ -22,7 +22,6 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import com.hazelcast.query.impl.bitmap.Bitmap;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
