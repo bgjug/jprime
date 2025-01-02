6c0c0a38e9f62aa036bc01e919d191b007ac0e6a
doychin <doychin@dsoft-bg.com>
2025-01-02 14:59:42 +0200

    Add branch and ticket price tables with initial data migration
    Introduce new `Branch` and `TicketPrice` tables to support branch-specific data management, with necessary schema and constraints. Add data migration scripts to seed initial branch records and preload logic adjustments to ensure proper initialization.
    
    Signed-off-by: doychin <doychin@dsoft-bg.com>

---
+++ src/main/resources/db/migration/data/V1.4.1__insert_branch_inital_data.sql
@@ -0,0 +1,20 @@
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2015, 'YEAR_2015', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2016, 'YEAR_2016', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2017, 'YEAR_2017', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2018, 'YEAR_2018', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2019, 'YEAR_2019', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2020, 'YEAR_2020', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2022, 'YEAR_2022', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2023, 'YEAR_2023', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2024, 'YEAR_2024', 0, 0, 1);
+insert into Branch(branch_year, label, agendaPublished, currentBranch, soldOut)
+values (2025, 'YEAR_2025', 0, 1, 0);
