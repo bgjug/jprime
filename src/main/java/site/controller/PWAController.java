078b24b57688dcc9e22e0cba395845767a1dd931
Nayden Gochev <gochev@gmail.com>
2025-11-19 17:16:29 +0200

    Fixing PWA controlelr to be invokable from PWA

--- src/main/java/site/controller/PWAController.java
+++ src/main/java/site/controller/PWAController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +20,7 @@ import site.repository.SessionRepository;
  */
 @Controller
 @RequestMapping("/pwa")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PWAController {
 
     private final SessionRepository sessionRepository;
